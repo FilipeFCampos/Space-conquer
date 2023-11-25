8de5a97d85a08304f45461b0f7a72ba52368d6ec
Filipe Campos <filipefcampos2005@gmail.com>
2023-11-25 15:40:45 -0300

    Fundo da tela inicial adicionado, botoes de iniciar e sair adicionados e cena 2d criada

---
+++ VideoStreamPlayer.gd
@@ -0,0 +1,20 @@
+extends VideoStreamPlayer
+
+func _init():
+	print("Jogo iniciado")
+
+func _on_finished():
+	play()
+
+
+func _on_exit_button_down():
+	get_tree().quit()
+	print("Jogo fechado")
+
+
+func _on_start_button_down():
+	get_tree().change_scene_to_file("res://Level_1.tscn")
+	print("Cena mudada")
+
+
+#func _on_start_focus_entered():
