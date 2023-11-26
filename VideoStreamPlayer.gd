8aa5e133fe50736f1fe28bc0c10723659bfec079
Filipe Campos <filipefcampos2005@gmail.com>
2023-11-26 12:54:41 -0300

    Animaçao de press dos botoes adicionada e tela cheia configurada

--- VideoStreamPlayer.gd
+++ VideoStreamPlayer.gd
@@ -7,10 +7,12 @@ func _on_finished():
 	play()
 
 func _on_exit_button_down():
+	await get_tree().create_timer(0.05).timeout
 	get_tree().quit()
 	print("Jogo fechado")
 
 
 func _on_start_button_down():
+	await get_tree().create_timer(0.05).timeout
 	get_tree().change_scene_to_file("res://Level_1.tscn")
 	print("Cena mudada")
