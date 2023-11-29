089139a52f3508b16671e2bde09c9f82ad374f1d
Filipe Campos <filipefcampos2005@gmail.com>
2023-11-29 14:59:35 -0300

    Inconsistencias corrigidas

--- personagem/player.gd
+++ personagem/player.gd
@@ -2,11 +2,12 @@ extends CharacterBody2D
 
 @export_category("Variables")
 @export var _move_speed: float = 64.0
-@export var _acceleration: float = 0.4
-@export var _friction: float = 0.8
+@export var _acceleration: float = 0.2
+@export var _friction: float = 0.2
 
 func _physics_process(_delta: float) -> void:
 	_move()
+	move_and_slide()
 
 func _move() -> void:
 	var _direction: Vector2 = Vector2(
@@ -14,7 +15,6 @@ func _move() -> void:
 		Input.get_axis("Cima", "Baixo")
 	)
 	
-<<<<<<< HEAD
 	if _direction != Vector2.ZERO:
 		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
 		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
@@ -22,9 +22,5 @@ func _move() -> void:
 		
 	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
 	velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _friction)
-=======
-	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
-	velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
->>>>>>> tela-inicial
 	velocity = _direction.normalized() * _move_speed
 	move_and_slide()
